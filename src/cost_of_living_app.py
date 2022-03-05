47543913727101c589f3fc20a14ca47de611ce26
Jiwei HU <90109585+hjw0703@users.noreply.github.com>
2022-03-05 12:03:28 -0800

    Update cost_of_living_app.py

--- src/cost_of_living_app.py
+++ src/cost_of_living_app.py
@@ -30,7 +30,7 @@ def plot1(city_name,cost_subset):
     return: A bar chart showing living cost in selected cities 
     """
     subset = data_df.loc[data_df["city"].isin(city_name),:]
-    y_title = list(price_subset.keys())[price_subset.values() == cost_subset]
+    y_title = list(price_subset.keys())[list(price_subset.values()).index(cost_subset)]
     chart = alt.Chart(subset).mark_bar().encode(
          alt.Y(cost_subset, title = f"{y_title}(USD)"),
          alt.X("city", title = "Cities", sort = "y", axis=alt.Axis(labelAngle=-45)),
@@ -79,7 +79,7 @@ def plot3(city_name, cost_subset):
     return: A choropleth map with point graph showing varying cost types in selected cities 
     """
     subset = data_df.loc[data_df["city"].isin(city_name),:]
-    y_title = list(price_subset.keys())[price_subset.values() == cost_subset]
+    y_title = list(price_subset.keys())[list(price_subset.values()).index(cost_subset)]
     world_map = alt.topo_feature(data.world_110m.url, feature ='countries')  
     map_click = alt.selection_multi(fields=['city'])
 
